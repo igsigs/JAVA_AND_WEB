723d2050bf06d2ecdeb7000bd307b74d47a623e0
igsigs <igsigs@sina.cn>
2017-05-30 12:21:50 +0800

    no message

--- Project/src/com/shiyanlou/duotai/Test.java
+++ Project/src/com/shiyanlou/duotai/Test.java
@@ -1,14 +1,13 @@
 package com.shiyanlou.duotai;
 
 public class Test {
-
 	/**
 	 * 主程序类
 	 */
 	public static void main(String[] args) {
 		Animal a = new Animal(); // 父类的引用指向本类的对象
 		Animal d = new Dog(); // 父类的引用指向子类的对象
-		Animal c = new Cat();
+		Animal c = new Cat();  // 父类的引用指向子类的对象
 		a.eat(); // 调用的是本类的方法
 		d.eat(); // 调用的是子类重写后的方法（程序会优先调用子类重写过的方法）
 		c.eat(); // 调用的是从父类继承过来的方法
