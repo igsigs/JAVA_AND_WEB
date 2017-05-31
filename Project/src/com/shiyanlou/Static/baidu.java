fb39ca1990ecd6d7be02311c1152d6c8d519a4c8
igsigs <igsigs@sina.cn>
2017-05-31 11:03:06 +0800

    no message

---
+++ Project/src/com/shiyanlou/Static/baidu.java
@@ -0,0 +1,8 @@
+package com.shiyanlou.Static;
+
+public class baidu {
+
+	public static void main(String[] args) {
+		System.out.println(company.name);
+	}
+}
---
+++ Project/src/com/shiyanlou/Static/company.java
@@ -0,0 +1,9 @@
+package com.shiyanlou.Static;
+
+/**
+ * 公司类
+ */
+public class company {
+	public static String name = "百度"; // 公司名字
+	public int num; // 员工的数量
+}
