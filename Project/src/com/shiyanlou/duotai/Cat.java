7cab4538c751df42374ea5240527377884ef88ab
igsigs <igsigs@sina.cn>
2017-07-28 16:34:18 +0800

    no message

--- Project/src/com/shiyanlou/duotai/Cat.java
+++ Project/src/com/shiyanlou/duotai/Cat.java
@@ -1,7 +1,8 @@
 package com.shiyanlou.duotai;
+
 /**
  * 猫类继承了动物类
  */
-public class Cat extends Animal{
-
+public class Cat extends Animal {
+	// 子类 Cat 什么都没有，只是单单的从父类继承了所有的东西过来
 }
--- Project/src/com/shiyanlou/duotai/Dog.java
+++ Project/src/com/shiyanlou/duotai/Dog.java
@@ -1,13 +1,15 @@
 package com.shiyanlou.duotai;
+
 /**
  * 狗类继承了动物类（即：继承是实现多态的基础）
  */
 public class Dog extends Animal {
 	@Override
-	public void eat(){
+	public void eat() { // 子类重写了从父类继承过来的方法
 		System.out.println("狗具有吃骨头的能力");
 	}
-	public void attack(){
+
+	public void attack() { // attack 是子类独有的方法，不是从父类继承过来的
 		System.out.println("狗具有攻击的能力");
 	}
 }
