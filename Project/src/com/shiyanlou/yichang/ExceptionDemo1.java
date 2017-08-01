276c5a8d19be91cf3de511232588552ac5baf61d
igsigs <igsigs@sina.cn>
2017-08-02 00:01:04 +0800

    no message

--- Project/src/com/shiyanlou/yichang/ExceptionDemo1.java
+++ Project/src/com/shiyanlou/yichang/ExceptionDemo1.java
@@ -10,6 +10,8 @@ public class ExceptionDemo1 {
 		Scanner scan = new Scanner(System.in);
 		int result = 0;
 		try {
+			//return; 程序会先执行 finally 然后在返回来执行 return 跳出方法
+			//System.exit(0); 直接结束包括 finally块
 			int number = scan.nextInt();
 			result = 10 / number;
 		} catch (InputMismatchException e) {
@@ -21,8 +23,10 @@ public class ExceptionDemo1 {
 		}catch(Exception e){
 			System.out.println(e.getMessage());
 			e.printStackTrace();
+		}finally{
+			//释放资源，比如关闭打开的文件，删除一些临时文件等等
+			System.out.println("输出结果为：" + result);	
 		}
-		System.out.println("输出结果为：" + result);
 	}
 
 }
